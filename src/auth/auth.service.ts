101a1a24240d34d022c36a469d5acbc4af13b4dc
YasiruWickramasinghe <it19038292@my.sliit.lk>
2023-08-09 20:08:45 +0530

    Signup Completed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
+    constructor(
+        private prisma: PrismaService) {}
 
-    signup() {
-        return 'this.authService.signup(dto);'
+    async signup(dto: AuthDto) {
+
+        // generate the password hash
+        const hash = await argon.hash(dto.password);
+
+        // save the new user in the db
+        try {
+        const user = await this.prisma.user.create({
+            data: {
+              email: dto.email,
+              hash,
+            },
+          });
+
+        //return user
+        return user;
+        } catch (error) {
+            if (
+              error instanceof
+              PrismaClientKnownRequestError
+            ) {
+              if (error.code === 'P2002') {
+                throw new ForbiddenException(
+                  'Credentials taken',
+                );
+              }
+            }
+            throw error;
+          }
     }
-    signin() {
+    signin(dto: AuthDto) {
         return 'this.authService.signin(dto);'
     }
 }
